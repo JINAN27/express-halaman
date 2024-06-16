cf1291110f440dba0781355f598a9a916d62414f
mmJinan <jinanfristaofficial27@gmail.com>
2024-06-16 13:03:05 +0700

    penambahan user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json([
         {id:1, name: 'johndoe'},
-        {id:2,  named: 'janedoen'}
+        {id:2,  named: 'janedoen'},
+        {id:3,  named: 'jinan'}
     ]);
 });
 
